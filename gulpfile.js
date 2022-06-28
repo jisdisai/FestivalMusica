4bbb34aff60ed2097278fcec3437bf2e536248fe
Isai Salgado <joaquin.salgado@ymail.com>
2022-06-28 14:05:26 -0600

    agregando funciones Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
-const {src, dest} = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
+const {src, dest, watch} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css(done){
 
-  src("src/scss/app.scss") // Identificar el archivo SASS
+  src('src/scss/**/*.scss') // Identificar el archivo SASS
+    .pipe(plumber())
     .pipe(sass()) // Compilarlo
-    .pipe(dest("build/css")) //Almacenarla en el Disco duro
+    .pipe(dest('build/css')) //Almacenarla en el Disco duro
 
   done();
+}
 
+function dev(done){
+  watch('src/scss/**/*.scss', css);
 
+  done();
 }
 
 exports.css = css;
+exports.dev = dev;
