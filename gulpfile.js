487bc04406692542347fd714551668868f2c0a4d
Isai Salgado <joaquin.salgado@ymail.com>
2022-07-18 11:16:18 -0600

    siguiendo con SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, parallel} = require('gulp');
+
+//css
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+//imagenes
+const cache = require('gulp-cache'); 
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function css(done){
 
@@ -12,6 +19,31 @@ function css(done){
   done();
 }
 
+function imagenes(done){
+
+  const  opciones = {
+    optimizationLevel: 3
+  }
+
+  src('src/img/**/*.{png,jpg}') 
+  .pipe(cache(imagemin(opciones)))
+  .pipe(dest('build/img'))
+done();
+}
+
+function versionWebp( done){
+
+  const opciones = {
+    quality:50
+  };
+
+  src('src/img/**/*.{png,jpg}')
+  .pipe(webp(opciones))
+  .pipe(dest('build/img'))
+
+  done();
+}
+
 function dev(done){
   watch('src/scss/**/*.scss', css);
 
@@ -19,4 +51,6 @@ function dev(done){
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, dev);
